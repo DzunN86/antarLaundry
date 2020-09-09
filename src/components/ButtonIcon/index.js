4b4b6c251f1d5dc6f11b640d87b4576e340ad970
DzunN86 <nurroinz@gmail.com>
2020-09-09 21:31:30 +0700

    add 2 icons

---
+++ src/components/ButtonIcon/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { StyleSheet, Text, TouchableOpacity } from 'react-native'
+import 
+
+const ButtonIcon = () => {
+    return (
+        <TouchableOpacity>
+            <Text>Button</Text>
+        </TouchableOpacity>
+    )
+}
+
+export default ButtonIcon
+
+const styles = StyleSheet.create({})
