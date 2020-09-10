208c501a28415a072f1b654a809368fd4fb49952
DzunN86 <nurroinz@gmail.com>
2020-09-11 01:02:25 +0700

    add all icon to index icons

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -6,5 +6,11 @@ import Akun from './akun.svg'
 import AkunActive from './akun-active.svg'
 import IconAddSaldo from './addSaldo.svg'
 import IconGetPoint from './getPoints.svg'
+import IconKiloan from './kiloan.svg'
+import IconVIP from './vip.svg'
+import IconSatuan from './satuan.svg'
+import IconEkspress from './ekspress.svg'
+import IconSetrika from './setrika.svg'
+import IconKarpet from './karpet.svg'
 
-export { Home, HomeActive, Pesanan, PesananActive, Akun, AkunActive, IconAddSaldo, IconGetPoint }
+export { Home, HomeActive, Pesanan, PesananActive, Akun, AkunActive, IconAddSaldo, IconGetPoint, IconKiloan, IconVIP, IconSatuan, IconEkspress, IconSetrika, IconKarpet }
